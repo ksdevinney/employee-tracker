2ae6246a2e4944e7cd36a876eda5288a6ac2ae0f
ksdevinney <katie@devinney.org>
2021-04-27 21:55:20 -0500

    work on inquirer prompts and begin databse

--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -5,22 +5,26 @@ CREATE DATABASE employeeDB;
 USE employeeDB;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10, 4) NOT NULL,
-  departmentId INT NOT NULL
+  departmentId INT NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   firstName VARCHAR(30) NOT NULL,
   lastName VARCHAR(30) NOT NULL,
   roleId INT NOT NULL,
-  managerId INT NULL
+  managerId INT NULL,
+  PRIMARY KEY (id)
 );
+
+INSERT INTO department (name) VALUES ("blorp"), ("doodle"), ("beep")
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const mysql = require('mysql');
+const util = require('util');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -12,7 +13,7 @@ const connection = mysql.createConnection({
     user: 'root',
   
     // Be sure to update with your own MySQL password!
-    password: '',
+    password: 'BigPeach',
     database: 'employeeDB',
   });
 
@@ -20,29 +21,92 @@ const connection = mysql.createConnection({
 // id
 // name
 
-function createDepartment() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'Department name?',
-            name: 'department'
-        },
-        {
-            name: 'number',
-            message: 'Enter the id number of the department.',
-            name: 'departmentId'
-        }
-    ])
+async function start() {
+    const response = await inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Team name?',
+        name: 'team',
+    }])
+    teamName = response.team;
+    await startMenu()
 }
+
+async function startMenu() {
+    const response = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                { value: 'department', name: 'Create a department' },
+                { value: 'role', name: 'Create a role' },
+                { value: 'employee', name: 'Create a employee' },
+                { value: 'exit', name: 'Begone' },
+            ],
+            name: 'startMenu'
+        }
+        // {
+        //     name: 'number',
+        //     message: 'Enter the id number of the department.',
+        //     name: 'departmentId'
+        // }
+    ])
+    // go to different function depending on response
+    console.log(response);
+    switch (response.startMenu) {
+        case 'department':
+            await createDepartment();
+            break;
+        case 'role':
+            await createRole();
+            break;
+        case 'employee':
+            await createEmployee();
+            break;
+        default:
+            console.log('Unexpected value!', response)
+    }
+};
+
+// const queryPromised = util.promisify(connection.query)
 
 // create role
 // id
 // title
 // salary
-// department
-function createRole() {
-    inquirer.prompt([
-        {
+// department ID
+async function createRole() {
+    const query = 'SELECT * FROM department';
+    // const replacements = [ answer.artist ];
+
+    // connection.query(query, replacements, (err, res) => {
+    //     // do stuff after query
+    // });
+
+    const res = await new Promise((resolve, reject) => {
+        connection.query(query, (err, res) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(res);
+            }
+            // do stuff after query
+        });
+    });
+    // do stuff after query
+
+    // const res = queryPromised(query);
+    // do stuff after query
+
+    if(!res.length) {
+        console.log('Sorry! No departments found!')
+        await startMenu();
+        return;
+    } 
+
+    const response = await inquirer.prompt([
+        {   
+            // not sure I need this?
             type: 'number',
             message: 'Enter the id number for this role.',
             name: 'roleId'
@@ -60,10 +124,12 @@ function createRole() {
         {
             type: 'list',
             message: 'Select the department for this role.',
-            choices: [department], //should be a list based on departments entered above
+            choices: res, //should be a list based on departments entered above
             name: 'roleDepartment'
         }
     ])
+
+console.log(response);
 }
 
 // create employee
@@ -72,8 +138,8 @@ function createRole() {
 // last name
 // role id (=6)
 // manager id (refers to another employee)
-function createEmployee() {
-    inquirer.prompt([
+async function createEmployee() {
+    const response = await inquirer.prompt([
         {
             type: 'number',
             message: 'Enter the id number for this employee.',
@@ -102,3 +168,9 @@ function createEmployee() {
         },
     ])
 }
+
+start().then(() => {
+    console.log('All done!');
+}).catch((error) => {
+    console.error('Whoops!', error);
+})
